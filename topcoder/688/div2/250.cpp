3edb7738a97d95eec875dd75726c2b90f9e5371a
Liuchenlong <lcl.maopao@gmail.com>
2016-06-17 20:18:07 +0800

    Create 250.cpp

---
+++ topcoder/688/div2/250.cpp
@@ -0,0 +1,76 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "ParenthesesDiv2Easy.cpp"
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <list>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+#include <ctime>
+using namespace std;
+#define eps 1e-8
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define ABS(a) ((a)<0?-(a):(a))
+
+
+class ParenthesesDiv2Easy
+{
+        public:
+        int getDepth(string s)
+        {
+                int ans=0;
+                int tnum=0;
+                for(int i=0;i<s.length();i++)
+                {
+                    if(s[i]=='(')
+                        tnum++;
+                    else
+                        tnum--;
+                    ans=max(ans,tnum);
+                }
+                return ans;
+        }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arg0 = "(())"; int Arg1 = 2; verify_case(0, Arg1, getDepth(Arg0)); }
+	void test_case_1() { string Arg0 = "()()()"; int Arg1 = 1; verify_case(1, Arg1, getDepth(Arg0)); }
+	void test_case_2() { string Arg0 = "(())()"; int Arg1 = 2; verify_case(2, Arg1, getDepth(Arg0)); }
+	void test_case_3() { string Arg0 = "((())())(((())(()))())"; int Arg1 = 4; verify_case(3, Arg1, getDepth(Arg0)); }
+	void test_case_4() { string Arg0 = "()"; int Arg1 = 1; verify_case(4, Arg1, getDepth(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+        ParenthesesDiv2Easy ___test;
+        ___test.run_test(-1);
+        return 0;
+}
+// END CUT HERE
