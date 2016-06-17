f9ea2c78e97206fb5fedd70de7bed32f3d8d8de2
Liuchenlong <lcl.maopao@gmail.com>
2016-06-17 20:42:31 +0800

    Update 250.cpp

--- topcoder/687/div2/250.cpp
+++ topcoder/687/div2/250.cpp
@@ -1,7 +1,7 @@
 // BEGIN CUT HERE
 
 // END CUT HERE
-#line 5 "Quacking.cpp"
+#line 5 "Quorum.cpp"
 #include <cstdlib>
 #include <cctype>
 #include <cstring>
@@ -31,124 +31,29 @@ using namespace std;
 #define MIN(a,b) ((a)<(b)?(a):(b))
 #define ABS(a) ((a)<0?-(a):(a))
 
-bool vis[2505];
-int get(char ch)
+
+class Quorum
 {
-    if(ch=='q')
-        return 0;
-    if(ch=='u')
-        return 1;
-    if(ch=='a')
-        return 2;
-    if(ch=='c')
-        return 3;
-    if(ch=='k')
-        return 4;
-}
-class Quacking
-{
-public:
-    int quack(string s)
-    {
-        memset(vis,0,sizeof(vis));
-        int ans=0;
-        while(1)
+        public:
+        int count(vector <int> arr, int k)
         {
-            int flag=0;
-            for(int i=0; i<s.length(); i++)
-                if(vis[i]==0)
-                {
-                    flag=1;
-                    break;
-                }
-            if(flag==0)
-                return ans;
-            int pos=0;
-            ans++;
-            int flag2=0;
-            for(int i=0;i<s.length();i++)
-            {
-                if(vis[i])
-                    continue;
-                if(get(s[i])==pos)
-                {
-                    flag2=1;
-                    vis[i]=1;
-                    pos=(pos+1)%5;
-                }
-            }
-            if(pos!=0||flag2==0)
-                return -1;
+                sort(arr.begin(),arr.end());
+                int sum=0;
+                for(int i=0;i<k;i++)
+                    sum+=arr[i];
+                return sum;
         }
-    }
 
 // BEGIN CUT HERE
-public:
-    void run_test(int Case)
-    {
-        if ((Case == -1) || (Case == 0)) test_case_0();
-        if ((Case == -1) || (Case == 1)) test_case_1();
-        if ((Case == -1) || (Case == 2)) test_case_2();
-        if ((Case == -1) || (Case == 3)) test_case_3();
-        if ((Case == -1) || (Case == 4)) test_case_4();
-        if ((Case == -1) || (Case == 5)) test_case_5();
-    }
-private:
-    template <typename T> string print_array(const vector<T> &V)
-    {
-        ostringstream os;
-        os << "{ ";
-        for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\",";
-        os << " }";
-        return os.str();
-    }
-    void verify_case(int Case, const int &Expected, const int &Received)
-    {
-        cerr << "Test Case #" << Case << "...";
-        if (Expected == Received) cerr << "PASSED" << endl;
-        else
-        {
-            cerr << "FAILED" << endl;
-            cerr << "\tExpected: \"" << Expected << '\"' << endl;
-            cerr << "\tReceived: \"" << Received << '\"' << endl;
-        }
-    }
-    void test_case_0()
-    {
-        string Arg0 = "quqacukqauackck";
-        int Arg1 = 2;
-        verify_case(0, Arg1, quack(Arg0));
-    }
-    void test_case_1()
-    {
-        string Arg0 = "kkkkk";
-        int Arg1 = -1;
-        verify_case(1, Arg1, quack(Arg0));
-    }
-    void test_case_2()
-    {
-        string Arg0 = "quackquackquackquackquackquackquackquackquackquack";
-        int Arg1 = 1;
-        verify_case(2, Arg1, quack(Arg0));
-    }
-    void test_case_3()
-    {
-        string Arg0 = "qqqqqqqqqquuuuuuuuuuaaaaaaaaaacccccccccckkkkkkkkkk";
-        int Arg1 = 10;
-        verify_case(3, Arg1, quack(Arg0));
-    }
-    void test_case_4()
-    {
-        string Arg0 = "quqaquuacakcqckkuaquckqauckack";
-        int Arg1 = 3;
-        verify_case(4, Arg1, quack(Arg0));
-    }
-    void test_case_5()
-    {
-        string Arg0 = "quackqauckquack";
-        int Arg1 = -1;
-        verify_case(5, Arg1, quack(Arg0));
-    }
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {5,2,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 2; verify_case(0, Arg2, count(Arg0, Arg1)); }
+	void test_case_1() { int Arr0[] = {1,1,1,1,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; int Arg2 = 5; verify_case(1, Arg2, count(Arg0, Arg1)); }
+	void test_case_2() { int Arr0[] = {50,2,9,49,38}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 49; verify_case(2, Arg2, count(Arg0, Arg1)); }
+	void test_case_3() { int Arr0[] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 14; int Arg2 = 105; verify_case(3, Arg2, count(Arg0, Arg1)); }
 
 // END CUT HERE
 
@@ -157,8 +62,8 @@ private:
 // BEGIN CUT HERE
 int main()
 {
-    Quacking ___test;
-    ___test.run_test(-1);
-    return 0;
+        Quorum ___test;
+        ___test.run_test(-1);
+        return 0;
 }
 // END CUT HERE
