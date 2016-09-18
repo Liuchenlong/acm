7247855b4999d508c5cd14c4c8c4223d53ee8420
Liuchenlong <lcl.maopao@gmail.com>
2016-09-18 11:58:31 +0800

    Create fastio.cpp

---
+++ Template/fastio.cpp
@@ -0,0 +1,8 @@
+inline int read()
+{
+    char ch;bool flag = false;int a = 0;
+    while(!((((ch = getchar()) >= '0') && (ch <= '9')) || (ch == '-')));
+    if(ch != '-'){a *= 10;a += ch - '0';}else{flag = true;}
+    while(((ch = getchar()) >= '0') && (ch <= '9')){a *= 10;a += ch - '0';}
+    if(flag)a = -a;return a;
+}
