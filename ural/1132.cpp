a328b6594d465f52ce714493c1b933370348bf63
Liuchenlong <lcl.maopao@gmail.com>
2016-09-22 09:45:32 +0800

    Create 1132.cpp

---
+++ ural/1132.cpp
@@ -0,0 +1,101 @@
+#include<cstdio>
+#include<cmath>
+#include<stdlib.h>
+#include<map>
+#include<set>
+#include<time.h>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+#define eps 1e-8
+
+long long qpow(long long x,long long k,long long mod)
+{
+    long long ans=1;
+    while(k)
+    {
+        if(k&1)ans=ans*x%mod;
+        k>>=1;
+        x=x*x%mod;
+    }
+    return ans;
+}
+struct Comp
+{
+    long long real,image;
+    Comp(){}
+    Comp(long long a,long long b)
+    {
+        real=a;
+        image=b;
+    }
+};
+long long w;
+Comp mult(Comp a,Comp b,long long mod)
+{
+    Comp ans;
+    ans.real=(a.real*b.real%mod+a.image*b.image%mod*w%mod)%mod;
+    ans.image=(a.real*b.image%mod+a.image*b.real%mod)%mod;
+    return ans;
+}
+Comp qpow(Comp x,long long k,long long mod)
+{
+    Comp ans=Comp(1,0);
+    while(k)
+    {
+        if(k&1)
+            ans=mult(ans,x,mod);
+        k>>=1;
+        x=mult(x,x,mod);
+    }
+    return ans;
+}
+long long legendre(long long a,long long p)
+{
+    return qpow(a,(p-1)/2,p);
+}
+long long quadratic_residue(long long n,long long p)
+{
+    if(p==2)return 1;
+    if(legendre(n,p)==p-1)return -1;
+    long long a=-1,t;
+    while(true)
+    {
+        a=rand()%p;
+        t=a*a-n;
+        w=(t%p+p)%p;
+        if(legendre(w,p)==p-1)break;
+    }
+    Comp ans=Comp(a,1);
+    ans=qpow(ans,(p+1)/2,p);
+    return ans.real;
+}
+
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    for(int cas=1;cas<=T;cas++)
+    {
+        long long n,p;
+        scanf("%I64d%I64d",&n,&p);
+        long long ans=quadratic_residue(n,p);
+        if(ans==-1)puts("No root");
+        else
+        {
+            long long a=ans;
+            long long b=p-ans;
+            if(a==b)printf("%I64d\n",a);
+            else
+            {
+                printf("%I64d %I64d\n",min(a,b),max(a,b));
+            }
+        }
+    }
+    return 0;
+}
